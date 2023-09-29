a63570b5c42469bea174c39190dd95e2916b97f7
JoseWenned <118996462+JoseWenned@users.noreply.github.com>
2023-09-29 17:00:22 -0300

    install dependencies

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ const app = express()
 
 app.use(express.json())
 
+app.get("/movies", )
+
 const PORT = 3000
 
 app.listen(PORT, () => {
---
+++ src/interfaces.ts
@@ -0,0 +1,7 @@
+export interface Movies {
+    id: number,
+    name: string,
+    category: string,
+    duration: number,
+    price: number
+}

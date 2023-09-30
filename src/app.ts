b473c0b48486b492741c8e2d2c32b7af7262aebd
JoseWenned <118996462+JoseWenned@users.noreply.github.com>
2023-09-30 00:21:55 -0300

    update

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
-import  express  from "express";
+import  express  from "express"
+import "dotenv/config"
+import { connectDatabase } from "./database"
+import { moviesRoutes } from "../src/routers/movies.routes"
 
 const app = express()
 
 app.use(express.json())
 
-app.get("/movies", )
+app.use("/movies", moviesRoutes)
 
 const PORT = 3000
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
+    connectDatabase()
 })
